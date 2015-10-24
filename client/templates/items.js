726f0373dc8cf1309fcb5b5d4a6c6ad4c7ff9cc7
Vasanth Pappu <vpappu@covermymeds.com>
2015-10-23 21:18:29 -0400

    redundant

--- client/templates/items.js
+++ client/templates/items.js
@@ -1,14 +1,3 @@
-Template.items.events({
-  "click .show-it": function (event) {
-    Meteor.call('showItems');
-    return false;
-  },
-  "click .hide-it": function (event) {
-    Meteor.call('hideItems');
-    return false;
-  }
-});
-
 Template.items.rendered = function () {
   $(document).ready(function () {
     $.material.init();
--- client/templates/newapp.js
+++ client/templates/newapp.js
@@ -1,14 +1,3 @@
-Template.newapp.events({
-  "click .show-it": function (event) {
-    Meteor.call('showItems');
-    return false;
-  },
-  "click .hide-it": function (event) {
-    Meteor.call('hideItems');
-    return false;
-  }
-});
-
 Template.newapp.rendered = function () {
   $(document).ready(function () {
     $.material.init();
